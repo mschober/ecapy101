4328fdcc6e10562649c3a4090405ba5d01ba6004
Michael Schober <mschober@expedia.com>
2015-12-28 08:41:20 -0800

    initial shapes

---
+++ shapes.py
@@ -0,0 +1,44 @@
+# Review
+# - lists
+# - for-each
+
+# Topics
+# - to string
+# - polymorphism
+
+# Key Terms
+# - class
+# - attribute
+# - method
+
+
+
+class Rect():
+    name = "Rectangle"
+
+    def __str__(self):
+        return self.name
+
+
+class Circ():
+    name = "Circle"
+
+    def __str__(self):
+        return self.name
+
+
+class Tri():
+    name = "Triangle"
+
+    def __str__(self):
+        return self.name
+
+
+shapes = [
+  Rect()
+, Circ()
+, Tri()
+]
+
+for shape in shapes:
+    print shape
